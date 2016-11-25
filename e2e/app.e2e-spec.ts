269aae78c14503978892e4ab93b1999a4ca6e949
Raphael Ferreira <johndoe@example.com>
2016-11-25 17:12:21 -0200

    Changed protractor config and sample test to work.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('ng2-mask-usage App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Teste:');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class Ng2MaskUsagePage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root span')).getText();
   }
 }
